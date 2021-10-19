4b8c7e59d97c654f9de8e3168ce4f0ff5797d5f9
tomwilson <tomas.f.wilson@gmail.com>
2021-10-19 07:28:00 -0300

    Fix list and shor user service

--- src/modules/users/infra/http/controller/UsersController.ts
+++ src/modules/users/infra/http/controller/UsersController.ts
@@ -1,3 +1,4 @@
+import ShowUserService from '@modules/users/services/ShowUserService';
 import { classToClass } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
@@ -6,9 +7,28 @@ import ListUserService from '../../../services/ListUserService';
 
 class UserControlerClass {
   public async index(req: Request, res: Response): Promise<Response> {
-    const users = await container.resolve(ListUserService).execute();
+    let search = '';
+    const sortField = String(req.query.sortField);
 
-    return res.json(classToClass({ data: users }));
+    if (req.query.search) {
+      search = String(req.query.search);
+    }
+
+    const listUser = container.resolve(ListUserService);
+
+    const users = await listUser.execute(search, sortField);
+
+    return res.json(classToClass(users));
+  }
+
+  public async show(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const showUser = container.resolve(ShowUserService);
+
+    const user = await showUser.execute({ id });
+
+    return res.json(user);
   }
 
   public async create(req: Request, res: Response): Promise<Response> {
